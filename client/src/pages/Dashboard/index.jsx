a8170abb74e48d279aa3dabd2dea675c1384d41d
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-07 10:27:06 +0700

    feat: add dashboard page

---
+++ client/src/pages/Dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import classes from './style.module.scss';
+import { FormattedMessage } from 'react-intl';
+
+export default function Dashboard() {
+    return (
+        <div className={classes.container}>
+            DASHBOARD
+        </div>
+    )
+};
